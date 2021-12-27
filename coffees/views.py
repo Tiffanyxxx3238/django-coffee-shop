0aeff5b5f27fc572f86120a2862227efbbf56c19
wenyu-chen <10756027@ntub.edu.tw>
2021-12-27 10:24:11 +0800

    feat(coffees):add delete page

--- coffees/views.py
+++ coffees/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 
+from utils.froms import DeleteConfirmForm
 from .forms import CoffeeForm
 from .models import Coffee
 
@@ -35,4 +36,11 @@ def edit(request, pk):
     return render(request, 'coffees/edit.html', {'form': form})
 
 
-
+def delete(request, pk):
+    coffee = get_object_or_404(Coffee, pk=pk)
+    form = DeleteConfirmForm(request.POST or None)
+    if form.is_valid() and form.cleaned_data['check']:
+        coffee.delete()
+        messages.success(request, '刪除成功')
+        return redirect('coffees:index')
+    return render(request, 'coffees/delete.html', {'form': form})
