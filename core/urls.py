c061bd60851729fb0690207b40e463ce23ec9c5c
wenyu-chen <10756027@ntub.edu.tw>
2022-01-21 15:51:41 +0800

    feat(coffees):add forget-password pge

--- core/urls.py
+++ core/urls.py
@@ -13,15 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetConfirmView
 from django.contrib import admin
 from django.shortcuts import redirect
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from utils.auth_views import register
 
 login_params = {
     'template_name': 'users/login.html',
     'redirect_authenticated_user': True,
+}
+
+password_reset_params = {
+    'template_name': 'users/password_reset.html',
+    'email_template_name': 'users/password_reset/email.html',
+    'subject_template_name': 'users/password_reset/subject.txt',
+    'success_url': reverse_lazy('login'),
+}
+password_set_params = {
+    'template_name': 'users/password_set.html',
+    'post_reset_login': True,
+    'success_url': reverse_lazy('login'),
 }
 
 urlpatterns = [
@@ -31,6 +43,16 @@ urlpatterns = [
     path('coffees/', include('coffees.urls')),
     path('admin/', admin.site.urls),
     path('register/', register, name='register'),
+    path(
+        'password-reset/',
+        PasswordResetView.as_view(**password_reset_params),
+        name='password_reset',
+    ),
+    path(
+        'password-set/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(**password_set_params),
+        name='password_set',
+    ),
 ]
 handler403 = 'utils.error_handlers.permission_denied'
 handler404 = 'utils.error_handlers.page_not_found'
