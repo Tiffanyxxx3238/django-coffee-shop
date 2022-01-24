9dca3c18228f12ba09953f5a4c4852d10669ed26
wenyu-chen <10756027@ntub.edu.tw>
2022-01-24 10:08:34 +0800

    feat(coffees):add permissions

--- coffees/views.py
+++ coffees/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 from utils.froms import DeleteConfirmForm
@@ -7,16 +8,19 @@ from .models import Coffee
 
 
 # Create your views here.
+@login_required
 def index(request):
     coffees = Coffee.objects.all()
     return render(request, 'coffees/index.html', {'coffees': coffees})
 
 
+@login_required
 def show(request, pk):
     coffee = get_object_or_404(Coffee, pk=pk)
     return render(request, 'coffees/show.html', {'coffee': coffee})
 
 
+@login_required
 def add(request):
     form = CoffeeForm(request.POST or None)
     if form.is_valid():
@@ -26,6 +30,7 @@ def add(request):
     return render(request, 'coffees/add.html', {'form': form})
 
 
+@login_required
 def edit(request, pk):
     coffee = get_object_or_404(Coffee, pk=pk)
     form = CoffeeForm(request.POST or None, instance=coffee)
@@ -36,6 +41,7 @@ def edit(request, pk):
     return render(request, 'coffees/edit.html', {'form': form})
 
 
+@login_required
 def delete(request, pk):
     coffee = get_object_or_404(Coffee, pk=pk)
     form = DeleteConfirmForm(request.POST or None)
